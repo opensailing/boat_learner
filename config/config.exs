1c95480df926a8c922e3762eabd3743a75e15104
Paulo Valente <16843419+polvalente@users.noreply.github.com>
2023-03-17 14:54:11 -0300

    chore: remove stray config entry

--- config/config.exs
+++ config/config.exs
@@ -4,7 +4,4 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
-# Use Jason for JSON parsing in Phoenix
-config :phoenix, :json_library, Jason
-
 import_config "#{config_env()}.exs"
