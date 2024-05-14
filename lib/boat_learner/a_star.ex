5d89a930396eb18bdf6b77dd1c293e5c77236b8f
Paulo Valente <16843419+polvalente@users.noreply.github.com>
2024-05-14 06:01:51 -0300

    wip: feature parity with elixir

--- lib/boat_learner/a_star.ex
+++ lib/boat_learner/a_star.ex
@@ -32,26 +32,37 @@ defmodule BoatLearner.AStar do
 
   @speed_kts [4.62, 5.02 | @speed_kts]
   @theta_deg [42.7, 137.6 | Enum.to_list(40..180//10)]
+
+  # @theta_deg Enum.to_list(40..180//10)
   @theta Enum.map(@theta_deg, &(&1 * @one_deg_in_rad))
   @speed Enum.map(@speed_kts, &(&1 * @kts_to_meters_per_sec))
+  @speed_over_ground_max Enum.max(@speed)
 
   defmodule PriorityQueue do
+    defstruct [:f_tree, :node_tree]
+
     def new do
-      :gb_trees.empty()
+      %__MODULE__{f_tree: :gb_trees.empty(), node_tree: :gb_trees.empty()}
     end
 
-    def put(queue, key, value) do
-      :gb_trees.insert(key, value, queue)
+    def put(queue, key, {f, node}) do
+      %__MODULE__{
+        f_tree: :gb_trees.insert(key, f, queue.f_tree),
+        node_tree: :gb_trees.insert(key, node, queue.node_tree)
+      }
     end
 
-    def put_if_cheaper(queue, key, value) do
-      case :gb_trees.lookup(queue, key) do
+    def put_if_cheaper(queue, key, {f, node}) do
+      case :gb_trees.lookup(key, queue.node_tree) do
         :none ->
-          put(queue, key, value)
+          put(queue, key, {f, node})
 
-        {:value, current} ->
-          if value < current do
-            :gb_trees.update(key, value, queue)
+        {:value, %{g: current_g}} ->
+          if node.g <= current_g do
+            %__MODULE__{
+              f_tree: :gb_trees.update(key, f, queue.f_tree),
+              node_tree: :gb_trees.update(key, node, queue.node_tree)
+            }
           else
             queue
           end
@@ -59,15 +70,24 @@ defmodule BoatLearner.AStar do
     end
 
     def pop(queue) do
-      if :gb_trees.is_empty(queue) do
+      if :gb_trees.is_empty(queue.f_tree) do
         {:error, :empty}
       else
-        {:ok, :gb_trees.take_smallest(queue)}
+        {key, f, f_tree} = :gb_trees.take_smallest(queue.f_tree)
+        {node, node_tree} = :gb_trees.take(key, queue.node_tree)
+
+        queue = %__MODULE__{
+          f_tree: f_tree,
+          node_tree: node_tree
+        }
+
+        {:ok, {key, {f, node}, queue}}
       end
     end
   end
 
   defmodule Node do
+    @derive {Inspect, only: [:x, :y, :f, :g, :heading, :tacking, :speed]}
     defstruct [:x, :y, :parent, f: 0, g: 0, h: 0, tacking: false, heading: 0, speed: 0]
 
     def equal?(this, that) do
@@ -94,14 +114,24 @@ defmodule BoatLearner.AStar do
       MapSet.member?(closed_list, key(node))
     end
 
-    def key(node), do: {node.x, node.y}
+    def key(node), do: {node.x, node.y, node.tacking}
   end
 
-  @speed_over_ground_max 10
-
   def solve({start_x, start_y}, {goal_x, goal_y}, opts \\ []) do
-    opts = Keyword.validate!(opts, [:start_heading])
-    open_list = :ets.new(:open_list, [:ordered_set])
+    opts =
+      Keyword.validate!(opts, [
+        :start_heading,
+        :boat_length,
+        :max_x,
+        :min_x,
+        :max_y,
+        :min_y,
+        :dt,
+        :grid_resolution,
+        :tacking_penalty
+      ])
+
+    open_list = PriorityQueue.new()
     closed_list = MapSet.new()
     polar_chart = init_polar_chart()
 
@@ -130,19 +160,42 @@ defmodule BoatLearner.AStar do
             |> Enum.reject(&Node.closed?(closed_list, &1))
             |> Enum.map(&update_neighbor_cost(&1, current_node, goal_node, opts))
 
-          Enum.reduce(neighbors, open_list, &Node.push_if_cheaper(&2, &1))
+          open_list = Enum.reduce(neighbors, open_list, &Node.push_if_cheaper(&2, &1))
+          solve_loop(open_list, closed_list, goal_node, opts)
         end
     end
   end
 
+  deftransformp wrap_phase(angles) when is_list(angles) do
+    two_pi = :math.pi() * 2
+
+    Enum.map(angles, fn angle ->
+      :math.fmod(:math.fmod(angle, two_pi) + two_pi, two_pi)
+    end)
+  end
+
+  deftransformp wrap_phase(angle) do
+    two_pi = Nx.multiply(pi(), 2)
+
+    angle
+    |> Nx.remainder(two_pi)
+    |> Nx.add(two_pi)
+    |> Nx.remainder(two_pi)
+  end
+
   def neighbors(node, goal_node, opts) do
-    angles = Enum.map(40..180//5, &(&1 * :math.pi() / 180))
-    angles = angles ++ Enum.map(angles, &(2 * :math.pi() - &1))
+    angles = Enum.map(40..180//1, &(&1 * :math.pi() / 180))
 
     angles =
-      Enum.filter(angles, fn angle ->
-        abs(angle_difference(angle, node.heading)) <= :math.pi() / 2
-      end)
+      if is_nil(node.parent) do
+        angles
+      else
+        Enum.filter(angles, fn angle ->
+          abs(angle_difference(angle, node.heading)) <= :math.pi() / 2
+        end)
+      end
+
+    angles = wrap_phase(angles ++ Enum.map(angles, &(2 * :math.pi() - &1)))
 
     case angles do
       [] ->
@@ -150,20 +203,23 @@ defmodule BoatLearner.AStar do
 
       angles ->
         dt = opts[:dt]
-        angles_t = Nx.tensor(angles)
+        angles_t = Nx.tensor(angles, type: :f64)
         speeds = speed_from_heading(opts[:polar_chart], angles_t)
         water_current = get_water_current(node.x, node.y, opts)
-        vmgs = vmg(node.position, goal_node.position, angles_t, speeds, water_current)
-        idx = Nx.argsort(vmgs, direction: :desc)[0..(min(Nx.size(vmgs), 2) - 1)]
-        tacking_angles = Nx.take(angles_t, idx)
-        angles = Nx.concatenate([Nx.reshape(node.heading, {1}), tacking_angles])
-        speeds = Nx.concatenate([Nx.reshape(node.speed, {1}), tacking_angles])
+        vmgs = vmg(node.x, node.y, goal_node.x, goal_node.y, angles_t, speeds, water_current)
 
-        speed_x = Nx.sin(angles) |> Nx.multiply(speeds) |> Nx.add(water_current[0])
-        speed_y = Nx.cos(angles) |> Nx.multiply(speeds) |> Nx.add(water_current[1])
+        idx = Nx.argsort(vmgs, direction: :desc)[0..1]
+        angles = Nx.concatenate([Nx.take(angles_t, idx), Nx.reshape(node.heading, {1})])
+        speeds = Nx.concatenate([Nx.take(speeds, idx), Nx.reshape(node.speed, {1})])
 
-        next_x = Nx.add(node.position[0], Nx.multiply(speed_x, dt))
-        next_y = Nx.add(node.position[1], Nx.multiply(speed_y, dt))
+        speed_x =
+          Nx.sin(angles) |> Nx.multiply(speeds) |> Nx.add(water_current[0])
+
+        speed_y =
+          Nx.cos(angles) |> Nx.multiply(speeds) |> Nx.add(water_current[1])
+
+        next_x = Nx.add(node.x, Nx.multiply(speed_x, dt))
+        next_y = Nx.add(node.y, Nx.multiply(speed_y, dt))
 
         next_pos = Nx.stack([next_x, next_y], axis: 1)
 
@@ -249,6 +305,7 @@ defmodule BoatLearner.AStar do
 
   defp update_neighbor_cost(next_node, current_node, goal_node, opts) do
     penalty = if(next_node.tacking, do: opts[:tacking_penalty], else: 0)
+
     g = current_node.g + opts[:dt] + penalty
     h = heuristic(next_node, goal_node)
     f = g + h
@@ -256,25 +313,25 @@ defmodule BoatLearner.AStar do
   end
 
   defp get_path(node, path \\ [])
-  defp get_path(%Node{parent: nil} = node, path), do: Enum.reverse([node | path])
-  defp get_path(node, path), do: get_path(node.parent, [node | path])
+  defp get_path(%Node{parent: nil} = node, path), do: [node | path]
+  defp get_path(node, path), do: get_path(node.parent, [%{node | parent: nil} | path])
 
   def init_polar_chart do
     # data for the boat at TWS=6
-    theta = Nx.tensor(@theta)
-    speed = Nx.tensor(@speed)
+    theta = Nx.tensor(@theta, type: :f64)
+    speed = Nx.tensor(@speed, type: :f64)
 
     spline_model = Scholar.Interpolation.BezierSpline.fit(theta, speed)
     # use the tail-end of the spline prediction as part of our linear model
-    dtheta = 0.1
+    dtheta = Nx.f64(0.1)
     min_theta = theta |> Nx.reduce_min() |> Nx.new_axis(0)
 
     dspeed =
       Scholar.Interpolation.BezierSpline.predict(spline_model, Nx.subtract(min_theta, dtheta))
 
     # Fit {0, 0}, {@dead_zone_angle, 0} and {min_theta, dspeed} as points for the linear "extrapolation"
-    dead_zone_thetas = Nx.tensor([0])
-    dead_zone_speeds = Nx.tensor([0])
+    dead_zone_thetas = Nx.tensor([0], type: :f64)
+    dead_zone_speeds = Nx.tensor([0], type: :f64)
 
     linear_model =
       Scholar.Interpolation.Linear.fit(
@@ -287,10 +344,11 @@ defmodule BoatLearner.AStar do
     {linear_model, spline_model, cutoff_angle}
   end
 
-  defnp speed_from_heading({linear_model, spline_model, cutoff_angle}, angle) do
+  defn speed_from_heading({linear_model, spline_model, cutoff_angle}, angle) do
     # angle is already maintained between 0 and 2pi
     # so we only need to calculate the "absolute value"
     # (as if the angle was between -pi and pi)
+    angle = Nx.as_type(wrap_phase(angle), :f64)
     angle = Nx.select(angle > pi(), 2 * pi() - angle, angle)
 
     linear_pred = Scholar.Interpolation.Linear.predict(linear_model, angle)
@@ -303,34 +361,25 @@ defmodule BoatLearner.AStar do
       |> Nx.reshape(Nx.devectorize(angle) |> Nx.shape())
       |> Nx.vectorize(angle.vectorized_axes)
 
-    cond do
-      angle <= @dead_zone_angle ->
-        0
-
-      angle <= cutoff_angle ->
-        linear_pred
-
-      true ->
-        spline_pred
-    end
+    (angle <= @dead_zone_angle)
+    |> Nx.select(0, Nx.select(angle <= cutoff_angle, linear_pred, spline_pred))
     |> Nx.clip(0, @speed_over_ground_max)
   end
 
-  defn vmg(current_pos, goal_pos, headings, speeds, water_current) do
-    headings = Nx.vectorize(headings, :headings)
-    speeds = Nx.vectorize(speeds, :speeds)
+  defn vmg(node_x, node_y, mark_x, mark_y, headings, speeds, water_current) do
+    velocity_vector =
+      Nx.stack([speeds * Nx.sin(headings), speeds * Nx.cos(headings)], axis: 1) + water_current
+
+    original_vec_axes = velocity_vector.vectorized_axes
 
     velocity_vector =
-      Nx.stack([speeds * Nx.sin(headings), speeds * Nx.cos(headings)]) + water_current
+      Nx.revectorize(velocity_vector, [collapsed: :auto], target_shape: {2})
 
-    velocity_vector = Nx.revectorize(velocity_vector, [collapsed: :auto], target_shape: {2})
-
-    direction_vector = goal_pos - current_pos
-    dot_product = Nx.dot(velocity_vector, direction_vector)
-
+    direction_vector = Nx.stack([mark_x - node_x, mark_y - node_y])
     direction_magnitude = Nx.LinAlg.norm(direction_vector)
 
-    Nx.devectorize(dot_product / direction_magnitude)
-    |> Nx.reshape({:auto})
+    vmgs = Nx.dot(velocity_vector, direction_vector) / direction_magnitude
+
+    Nx.revectorize(vmgs, original_vec_axes, target_shape: {:auto})
   end
 end
